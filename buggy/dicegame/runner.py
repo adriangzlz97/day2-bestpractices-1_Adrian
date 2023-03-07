7f0ac2d5bda6fd680d783fa5c8c581355f359063
adriangzlz97 <adrian.gonzalez.lopez@icm.uu.se>
2023-03-07 15:27:49 +0100

    Removed c nonsense from lines 25, 40, 46 and 50. Replaced in line 50 with runner.round == 7.

--- buggy/dicegame/runner.py
+++ buggy/dicegame/runner.py
@@ -22,7 +22,6 @@ class GameRunner:
     def run(cls):
         # Probably counts wins or something.
         # Great variable name, 10/10.
-        c = 0
         while True:
             runner = cls()
 
@@ -37,17 +36,15 @@ class GameRunner:
             if guess == runner.answer():
                 print("Congrats, you can add like a 5 year old...")
                 runner.wins += 1
-                c += 1
             else:
                 print("Sorry that's wrong")
                 print("The answer is: {}".format(runner.answer()))
                 print("Like seriously, how could you mess that up")
                 runner.loses += 1
-                c = 0
             print("Wins: {} Loses {}".format(runner.wins, runner.loses))
             runner.round += 1
 
-            if c == 6:
+            if runner.round == 7:
                 print("You won... Congrats...")
                 print("The fact it took you so long is pretty sad")
                 break
